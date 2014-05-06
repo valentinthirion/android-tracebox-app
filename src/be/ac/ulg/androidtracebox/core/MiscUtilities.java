80c086264a91337c48f6069ada225b086f841715
Valentin THIRION <valentin@medineo.be>
2014-05-06 21:57:29 +0200

    Added the copyright text

--- src/be/ac/ulg/androidtracebox/core/MiscUtilities.java
+++ src/be/ac/ulg/androidtracebox/core/MiscUtilities.java
@@ -1,3 +1,15 @@
+/*
+ * Tracebox for Android has been developed by Valentin THIRION
+ * in the context of his Master Thesis
+ * at the University of Liege (ULg) in Belgium in june 2014.
+ * This work has been partially funded by the
+ * European Commission funded mPlane ICT-318627 project
+ * (http://www.ict-mplane.eu).
+ * 
+ * All information, copyrights and code about
+ * this project can be found at: www.androidtracebox.com
+ */
+
 package be.ac.ulg.androidtracebox.core;
 
 import android.content.Context;
